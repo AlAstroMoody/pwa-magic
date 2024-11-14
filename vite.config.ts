575141668d6ad6c4dec84a4e5d7d5f3f6f3195c2
alex <aastrotenko@mail.ru>
2024-11-14 21:53:41 +0700

    больше исправлений для нормального деплоя

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pwa-magic',
+  base: '/pwa-magic/',
   plugins: [
     vue(),
+    tsconfigPaths(),
     vueDevTools(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: 'auto',
       // devOptions: {
       //   enabled: true,
       // },
