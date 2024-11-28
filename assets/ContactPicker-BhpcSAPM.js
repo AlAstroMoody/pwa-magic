083a130281f70de59e7a2f58e27b8729733cb13e
alex <aastrotenko@mail.ru>
2024-11-28 23:37:40 +0700

    Updates

---
+++ assets/ContactPicker-BhpcSAPM.js
@@ -0,0 +1,1 @@
+import{d as e,o as c,c as o}from"./index-DNm80A1y.js";const i=e({__name:"ContactPicker",setup(a){async function t(){try{await navigator.contacts.getProperties()}catch{alert("Не поддерживается на устройстве")}}return(n,r)=>(c(),o("button",{onClick:t},"Click"))}});export{i as default};
