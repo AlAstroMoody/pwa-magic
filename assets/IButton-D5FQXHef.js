4e0659f841ff2b2038e46fbd6e532c676848bb82
alex <aastrotenko@mail.ru>
2024-11-30 22:16:23 +0700

    Updates

--- assets/IButton-D5FQXHef.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as e,c as s,r as n}from"./index-Cl5dxKTN.js";const c={},r={class:"button"};function a(t,_){return e(),s("button",r,[n(t.$slots,"default")])}const f=o(c,[["render",a]]);export{f as i};
